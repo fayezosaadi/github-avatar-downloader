290e22c8b2fe15408555fff1a4b1d71f67d3d4ab
fayezosaadi <fayezosaadi@gmail.com>
2017-04-11 03:17:29 +0000

    Add .pipe Function to Downlad the pictures

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,13 @@
   const request = require('request');
-  const GITHUB_USER = "fayezosaadi";
-  const GITHUB_TOKEN = "6a407681036e7c8da1759b16ca9008504634c425";
+  const fs = require('fs');
+  const config = require ('./config');
   console.log('Welcome to the GitHub Avatar Downloader!');
 
 
   function getRepoContributors(repoOwner, repoName, cb) {
 
 
-    let requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+    let requestURL = `https://${config.GITHUB_USER}:${config.GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
       console.log(requestURL);
 
       requestOptions = {
@@ -26,10 +26,25 @@
 function callback (err, result) {
 if (!err)
   var avatarUrl=JSON.parse(result.body);
-  avatarUrl.forEach((e) => { console.log("Result:", e.avatar_url)})
+  avatarUrl.forEach((e) => { downloadImageByURL(e.avatar_url, './avatars/' + e.login + '.jpg')})
 }
 
 let repOwner = process.argv[2];
 let repName =  process.argv[3];
 
 getRepoContributors(repOwner, repName, callback);
+
+
+
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log("file Downloades");
+  })
+  .pipe(fs.createWriteStream(filePath));
+ }
