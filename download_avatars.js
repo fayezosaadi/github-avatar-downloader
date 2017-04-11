eeec66b3864703b6f3b55d98894c6e66db858648
fayezosaadi <fayezosaadi@gmail.com>
2017-04-11 04:02:44 +0000

    Refactor the Code

--- download_avatars.js
+++ download_avatars.js
@@ -1,50 +1,53 @@
   const request = require('request');
   const fs = require('fs');
   const config = require ('./config');
+  let repOwner = process.argv[2];
+  let repName =  process.argv[3];
+
   console.log('Welcome to the GitHub Avatar Downloader!');
 
-
   function getRepoContributors(repoOwner, repoName, cb) {
-
-
+    if (!repoOwner){
+      console.log("please write down a valid value Repos Owner");
+      return;
+    }
     let requestURL = `https://${config.GITHUB_USER}:${config.GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-      console.log(requestURL);
-
+      // console.log(requestURL);
       requestOptions = {
-        headers:{"User-Agent":"FAYEZ"},
-        url: requestURL
-      }
-
-      request.get(requestOptions, cb)
-
-        .on('response', (response) => {
-    // console.log(response.headers);
-        });
-      }
+      headers:{"User-Agent":"FAYEZ"},
+      url: requestURL
+    }
+    request.get(requestOptions, cb)
+      .on('response', (response) => {
+      // console.log(response.headers);
+      });
+  }
 
 
-function callback (err, result) {
-if (!err)
-  var avatarUrl=JSON.parse(result.body);
-  avatarUrl.forEach((e) => { downloadImageByURL(e.avatar_url, './avatars/' + e.login + '.jpg')})
-}
+  function callback (err, result) {
+    if (!err)
+      var avatarUrl=JSON.parse(result.body);
+    avatarUrl.forEach((e) => { downloadImageByURL(e.avatar_url, './avatars/' + e.login + '.jpg')})
+  }
 
-let repOwner = process.argv[2];
-let repName =  process.argv[3];
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+    console.log("Downloading pictires");
 
-getRepoContributors(repOwner, repName, callback);
+    })
+    .pipe(fs.createWriteStream(filePath));
+  }
+
+
+
+  getRepoContributors(repOwner, repName, callback);
 
 
 
 
-function downloadImageByURL(url, filePath) {
 
-  request.get(url)
-  .on('error', function (err) {
-    throw err;
-  })
-  .on('response', function (response) {
-    console.log("file Downloades");
-  })
-  .pipe(fs.createWriteStream(filePath));
- }
+
