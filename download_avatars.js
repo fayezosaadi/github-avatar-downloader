b176686d80a39bd614beff0343028c2ecc91afe0
fayezosaadi <fayezosaadi@gmail.com>
2017-04-11 02:19:47 +0000

    Get http request

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,36 @@
-  var request = require('request');
+  const request = require('request');
+  const GITHUB_USER = "fayezosaadi";
+  const GITHUB_TOKEN = "6a407681036e7c8da1759b16ca9008504634c425";
   console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+  function getRepoContributors(repoOwner, repoName, cb) {
+
+
+    var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+      console.log(requestURL);
+
+
+      requestOptions = {
+        headers:{"User-Agent":"FAYEZ"},
+        url: requestURL
+      }
+
+      request.get(requestOptions, cb)
+
+        .on('response', (response) => {
+// console.log(response.headers);
+        });
+      }
+
+
+function callback (err, result) {
+if (!err);
+  console.log("Result:", result.body);
+};
+
+
+let repOwner = process.argv[2];
+let repName =  process.argv[3];
+
+getRepoContributors(repOwner, repName, callback);
